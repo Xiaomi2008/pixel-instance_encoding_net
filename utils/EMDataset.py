4bd61050721d565f85c5082991f69d383ee8605d
Tao Zeng <tzeng@yahoo-inc.com>
2017-10-24 02:54:30 -0700

    add EMdataset class

--- utils/EMDataset.py
+++ utils/EMDataset.py
@@ -63,9 +63,9 @@ class CRIME_Dataset(Dataset):
       self.V = volumes[data_name[self.dataset]]
       gradX = np.array(self.V['gradX_dataset']).astype(np.double)
       gradY = np.array(self.V['gradY_dataset']).astype(np.double)
-      gradX = np.expand_dims(gradX, -1)
-      gradY = np.expand_dims(gradY, -1)
-      self.gd_data = np.concatenate(gradX,gradY,axis=-1)
+      gradX = np.expand_dims(gradX, 1)
+      gradY = np.expand_dims(gradY, 1)
+      self.gd_data = np.concatenate(gradX,gradY,axis=1)
       self.lb_data = np.array(self.V.data_dict['label_dataset']).astype(np.int32)
       self.im_data = np.array(self.V.data_dict['image_dataset']).astype(np.int32)
 
