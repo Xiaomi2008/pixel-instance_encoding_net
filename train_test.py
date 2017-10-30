ad93ffe2df52ccf301e3952af851ea879dc1e91b
Tao Zeng <tzeng@yahoo-inc.com>
2017-10-30 00:45:48 -0700

    te

--- train_test.py
+++ train_test.py
@@ -265,9 +265,9 @@ def create_model(model_name, input_size =224, pretrained_iter=None):
         model = GCN(num_classes=2, input_size=input_size).double()
     elif model_name == 'Unet':
         model = Unet()
-        
+
     if  pretrained_iter:
-        model_file = model_saved_dir +'/' +'{}_size224_iter_{}.model'.format(model_name,pre_trained_iter)
+        model_file = model_saved_dir +'/' +'{}_size224_iter_{}.model'.format(model_name,pretrained_iter)
         #model_file =model_saved_dir + '/' + 'GCN_size224_iter49499.model'
         #model_file = model_saved_dir +'/' +'{}_instance_grad_iter_{}.model'.format(model_name,pre_trained_iter)
     else:
