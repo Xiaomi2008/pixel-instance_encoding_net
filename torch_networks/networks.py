57a4d763a4b0957baa836f4776c30d2a6c4f1834
Xiaomi2008 <zengtao08@gmail.com>
2017-11-24 19:41:21 -0800

    u

--- torch_networks/networks.py
+++ torch_networks/networks.py
@@ -116,9 +116,9 @@ class Unet(nn.Module):
         self.down_block_5 = Downblock(b5_down_ch,num_conv_in_block,1,kernel_size)
 
         b0_up_ch = b5_down_ch * 1
-        self.up_block_0 = Upblock(b0_up_ch+b4_down_ch,num_conv_in_block,1,kernel_size)
+        self.up_block_0 = Upblock(b0_up_ch+b5_down_ch,num_conv_in_block,2,kernel_size)
 
-        b1_up_ch = b4_down_ch * ch_change_rate
+        b1_up_ch = b5_down_ch * 1
         self.up_block_1 = Upblock(b1_up_ch+b4_down_ch,num_conv_in_block,ch_change_rate,kernel_size)
 
         b2_up_ch = b1_up_ch // ch_change_rate
